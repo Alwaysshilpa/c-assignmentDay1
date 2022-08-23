704026edcd4c870fa0c0905cf8657ada96b0e696
252-Shilpa P V <110774129+Alwaysshilpa@users.noreply.github.com>
2022-08-23 05:26:27 -0700

    Add files via upload

---
+++ swaping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class swaping
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            a = a * b;
+            b = a / b;
+            a = a / b;
+        }
+        public static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine($"Before swap a= {a} b={b}");
+            Swap(ref a, ref b);
+            Console.WriteLine($"Before swap a= {a} b={b}");
+        }
+    }
+}
